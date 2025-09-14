928a2e70cf709c845e9ce82ba4fd7d1148f3b30b
Abdi Guled <133576011+AhmedGuled@users.noreply.github.com>
2025-09-14 13:39:09 -0400

    Navbar,auth

---
+++ hanti/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="#">Buy</a> |
+            <a href="#">Rent</a> |
+            <a href="#">Sell</a> |
+            <a href="#">List Rentals</a> |
+            <a href="#">See Agents</a> |
+            <a href="#">Sign In</a>
+            <br />
+            <a href="#">Join Us Now</a>
+        </nav>
+    );
+}
